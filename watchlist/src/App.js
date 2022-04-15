31aa4e5038c006e33d7e37a46b83b1cf4b45d942
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-04-15 19:44:20 -0400

    import lib folder and setup router-dom

--- watchlist/src/App.js
+++ watchlist/src/App.js
@@ -1,7 +1,16 @@
-import './styles/App.css';
+import './styles/App.scss';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Homepage from './pages/Homepage';
 
 function App() {
-  return <div className="App">Appppppp</div>;
+  return (
+    <Router>
+      <div className="App">
+        <Route path="/" page={Homepage} />
+        {/* <Route path="" component={} /> */}
+      </div>
+    </Router>
+  );
 }
 
 export default App;
