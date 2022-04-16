2e42388b03dcba9bfda86c04c7a94b62328f26f8
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-04-15 20:09:55 -0400

    install react-router v 5.2.0

--- watchlist/src/App.js
+++ watchlist/src/App.js
@@ -1,13 +1,19 @@
 import './styles/App.scss';
+import './lib/font-awesome/css/all.min.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Homepage from './pages/Homepage';
+import Header from './components/Header';
+import Watchlist from './pages/Watchlist';
+import Watched from './pages/Watched';
+import Add from './pages/Add';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route path="/" page={Homepage} />
-        {/* <Route path="" component={} /> */}
+        <Header />
+        <Route exact path="/" page={Watchlist} />
+        <Route path="/watched" page={Watched} />
+        <Route path="/add" page={Add} />
       </div>
     </Router>
   );
--- watchlist/src/components/Header.jsx
+++ watchlist/src/components/Header.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container">
+        <div className="content">
+          <div className="brand">
+            <Link to="/">Watchlist</Link>
+          </div>
+          <ul className="nav-links">
+            <li>
+              <Link to="/">Watch List</Link>
+            </li>
+            <li>
+              <Link to="/watched">Watched</Link>
+            </li>
+            <li>
+              <Link to="/add" className="btn">
+                + Add
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
