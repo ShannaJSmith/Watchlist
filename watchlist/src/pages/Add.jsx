1e7df5a6166ea809b8c123ab5d9c1165fd11456f
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-04-17 19:41:39 -0400

    implement query state with onChange function

--- watchlist/src/pages/Add.jsx
+++ watchlist/src/pages/Add.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import '../styles/App.scss';
 
 const Add = () => {
+  const [query, setQuery] = useState('');
+
+  const onChange = (e) => {
+    e.preventDefault();
+
+    setQuery(e.target.value);
+  };
+
   return (
     <div className="add-page">
       <div className="container">
         <div className="add-content">
           <div className="input-wrapper">
-            <input type="text" placeholder="Search for an anime..." />
+            <input
+              type="text"
+              placeholder="Search for an anime..."
+              value={query}
+              onChange={onChange}
+            />
           </div>
         </div>
       </div>
