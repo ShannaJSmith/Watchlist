b71d2e498e9f0843cfec2eb5468469ab50ae6dad
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-04-18 14:47:28 -0400

    implement persisting data in localstorage

--- watchlist/src/context/GlobalState.jsx
+++ watchlist/src/context/GlobalState.jsx
@@ -4,8 +4,14 @@ import AppReducer from './AppReducer';
 // inital state
 
 const initialState = {
-  watchlist: [],
-  watched: [],
+  // when app loads intitial state will be empty if nothing is stored in localstorage
+  // if data is in localstorage. convert it back into an array with json.parse
+  watchlist: localStorage.getItem('watchlist')
+    ? JSON.parse(localStorage.getItem('watchlist'))
+    : [],
+  watched: localStorage.getItem('watched')
+    ? JSON.parse(localStorage.getItem('watched'))
+    : [],
 };
 
 // create context
@@ -15,6 +21,13 @@ export const GlobalContext = createContext(initialState);
 // provider components
 export const GlobalProvider = (props) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  // triggered every time state is changed (e.g. movie added to watchlist)
+  useEffect(() => {
+    // localstorage must be a string
+    localStorage.setItem('watchlist', JSON.stringify(state.watchlist));
+    localStorage.setItem('watched', JSON.stringify(state.watched));
+  }, [state]);
 
   // actions (tells app what to do after addmovie button is clicked)
   const addMovieToWatchlist = (movie) => {
