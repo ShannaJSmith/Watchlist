2f3c777d28685f19661c042d0393c72f1e9d840d
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-04-18 13:12:22 -0400

    implement fetch request to set results and display in ul

--- watchlist/src/pages/Add.jsx
+++ watchlist/src/pages/Add.jsx
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
-// import '../styles/App.scss';
-// import axios from 'axios';
 
 const Add = () => {
   const [query, setQuery] = useState('');
+  const [results, setResults] = useState([]);
 
   const onChange = (e) => {
     e.preventDefault();
@@ -11,11 +10,16 @@ const Add = () => {
     setQuery(e.target.value);
 
     fetch(
-      `https://api.themoviedb.org/3/movie/550?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1&include_adult=false&query=${e.target.value}`
+      `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1&include_adult=false&query=${e.target.value}`
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log('data', data);
+        // console.log('data', data);
+        if (!data.errors) {
+          setResults(data.results);
+        } else {
+          setResults([]);
+        }
       });
   };
 
@@ -26,11 +30,18 @@ const Add = () => {
           <div className="input-wrapper">
             <input
               type="text"
-              placeholder="Search for an anime..."
+              placeholder="Search for a movie..."
               value={query}
               onChange={onChange}
             />
           </div>
+          {results.length > 0 && (
+            <ul className="results">
+              {results.map((movie) => (
+                <li>{movie.title}</li>
+              ))}
+            </ul>
+          )}
         </div>
       </div>
     </div>
