f34119365b80b2bde42a7083358b0f881fe69908
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-04-21 12:41:02 -0400

    convert react app to react-rputer dom 17 6 and react dom 17

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import './styles/App.scss';
 import './lib/font-awesome/css/all.min.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Watchlist from './pages/Watchlist';
 import Watched from './pages/Watched';
 import Add from './pages/Add';
 import { GlobalProvider } from './context/GlobalState';
+import React from 'react';
 
 function App() {
   return (
     <GlobalProvider>
-      <Router>
-        <div className="App">
-          <Header />
-          <Route exact path="/" component={Watchlist} />
-          <Route path="/watched" component={Watched} />
-          <Route path="/add" component={Add} />
-        </div>
-      </Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Watchlist />} />
+          <Route path="/watched" element={<Watched />} />
+          <Route path="/add" element={<Add />} />
+        </Routes>
+      </div>
     </GlobalProvider>
   );
 }
